ebb4f9dd37c9935c8e5cf9ac7113c9e366fa9479
dzwiener <dzwiener4@icloud.com>
2022-07-21 12:29:54 -0500

    Merging with Main

--- src/components/RegressiveSearchPage/RegressiveSearchPage.js
+++ src/components/RegressiveSearchPage/RegressiveSearchPage.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import unionBy from 'lodash/unionBy'
 import { decodeRecessiveGenesAndNormalize } from '@thanpolas/degenking/src/heroes-helpers/recessive-genes.ent'
 import { getHeroDataByAuction } from '../../services/auction.service'
 import { calcuateSummonCost } from '../../helpers/prices.helper'
 import { getProbabilityThatHeroesCanSummonTargetGene, getPossibleSummonClasses } from '../../helpers/genes.helpers'
 import { CamelCase, ToPrice } from '../../helpers/format.helpers'
-import { getMainHero, sheerFix, sortAndFilterHeroes } from './functions'
-import { REALMS } from '../../constants/realms'
+import { getMainHero, sheerFix, sortAndFilterHeroes, toCrystalValeHeroId } from './functions'
+import { NETWORKS, REALMS } from '../../constants/realms'
 
-import Alert from '@mui/material/Alert'
 import Container from '@mui/material/Container'
-import HeroSnapshot from '../HeroSnapshot'
+import LoadingMessage from './LoadingMessage'
+import MainHero from './MainHero'
 import SortFilter from '../SortFilter/SortFilter'
 import SearchFormSimple from '../SearchFormSimple/SearchFormSimple'
 import SummonsMatchList from '../SummonsMatchList'
@@ -25,35 +25,32 @@ const statusMessages = [
     'Woah, big guy!  Careful with those horns!'
 ]
 
-const MainHero = ({ hero, view }) => {
-    return hero ?
-        (<div className='main-hero'>
-            <HeroSnapshot hero={hero} title='Main Hero' view={view} />
-        </div>) :
-        null
-}
-
-const LoadingMessage = ({ heroCount, loaded, loading, message }) => {
-    const textToDisplay = loading ? message : loaded && !heroCount ? 'No Heroes Found' : null
-    return textToDisplay ? (
-        <Alert variant='outlined' severity='success' className='loading-message'>
-            {textToDisplay}
-        </Alert>
-    ) : null
-}
-
 const RegressiveSearchPage = () => {
-    const [isLoading, setIsLoading] = useState(false)
+    const [filters, setFilters] = useState({})
     const [hasLoaded, setHasLoaded] = useState(false)
+    const [heroes, setHeroes] = useState([])
+    const [highlights, setHighlights] = useState()
+    const [isLoading, setIsLoading] = useState(false)
     const [loadingMessage, setLoadingMessage] = useState('')
+    const [mainHero, setMainHero] = useState()
+    const [searchCriteria, setSearchCriteria] = useState({
+        auctionType: 'assisting',
+        heroId: '',
+        network: NETWORKS.harmony.id,
+        originRealm: REALMS.serendale.id,
+        summonClass: '',
+        summonProfession: 'all',
+        walletAddresses: []
+    })
     const [searchForm, setSearchForm] = useState('simple')
     const [sortBy, setSortBy] = useState('probability')
-    const [filters, setFilters] = useState({})
-    const [mutationClass, setMutationClass] = useState('')
-    const [mainHero, setMainHero] = useState()
-    const [heroes, setHeroes] = useState([])
     const [view, setView] = useState('back')
-    const [highlights, setHighlights] = useState()
+
+    // Update the hero if the network changes
+    useEffect(() => {
+        searchCriteria.heroId && handleHeroChange()
+        // eslint-disable-next-line
+    }, [searchCriteria.originRealm])
 
     const delay = ms => new Promise(res => setTimeout(res, ms))
 
@@ -68,13 +65,17 @@ const RegressiveSearchPage = () => {
     }
 
     // Looks up the selected Hero
-    const handleHeroChange = async (heroNumber, originRealm) => {
-        const heroId = originRealm === REALMS.serendale.id ? heroNumber : `${'1000000000000'.slice(0, 13 - heroNumber.length)}${heroNumber}`
+    const handleHeroChange = async () => {
+        const heroNumber = searchCriteria.heroId
+        const heroId = searchCriteria.originRealm === REALMS.serendale.id
+            ? heroNumber
+            : toCrystalValeHeroId(heroNumber)
+
         if (heroId && (!mainHero || mainHero.id !== heroId)) {
             // Clear currently displayed main and matching heroes
             setHeroes([])
             setMainHero()
-            setMutationClass('')
+            handleSearchCriteriaChange({ key: 'summonClass', value: '' })
             setLoadingMessage('')
 
             // Retrieve the main hero from DFK
@@ -84,12 +85,18 @@ const RegressiveSearchPage = () => {
                 setMainHero(_mainHero)
 
                 // Set a default for the class to summon based on the selected hero
-                setMutationClass(_mainHero.mutationClass)
+                _mainHero.mutationClass &&
+                    handleSearchCriteriaChange({ key: 'summonClass', value: _mainHero.mutationClass })
             } else {
                 setLoadingMessage('Hero not found.')
                 setIsLoading(true)
             }
         }
+    }
+
+    // Updates search criteria
+    const handleSearchCriteriaChange = ({ key, value }) => {
+        setSearchCriteria({ ...searchCriteria, [key]: value })
     }
 
     // Updates sorting options
@@ -98,7 +105,7 @@ const RegressiveSearchPage = () => {
     }
 
     // Creates a new search for the specified search criteria
-    const handleSubmit = async searchCriteria => {
+    const handleSearch = async () => {
         setHeroes([])
         setIsLoading(true)
         setRandomLoadingMessage()
@@ -114,7 +121,7 @@ const RegressiveSearchPage = () => {
 
         while (!isLastPage) {
             // Retrieve a page of hero listings from tavern
-            const pageOfListings = await getHeroDataByAuction(searchCriteria.auctionType, searchCriteria.network, classes, searchCriteria.summonProfession, searchCriteria.walletAddress, pageSize, offset)
+            const pageOfListings = await getHeroDataByAuction(searchCriteria.auctionType, searchCriteria.network, classes, searchCriteria.summonProfession, searchCriteria.walletAddresses, pageSize, offset)
             const listedHeroes = decodeRecessiveGenesAndNormalize(pageOfListings.heroes)
 
             // Analyze each of the heroes in auction
@@ -127,11 +134,12 @@ const RegressiveSearchPage = () => {
                 heroToAnalyze.price = ToPrice(heroToAnalyze.price)
                 const classProbability = getProbabilityThatHeroesCanSummonTargetGene(mainHero.mainClassGenes, heroToAnalyze.mainClassGenes, searchCriteria.summonClass)
 
-                if (searchCriteria.summonProfession) {
+                // Apply the probability based on search criteria
+                if (searchCriteria.summonProfession === 'all') {
+                    heroToAnalyze.targetProbability = classProbability ? classProbability.value : 0
+                } else {
                     const professionProbability = getProbabilityThatHeroesCanSummonTargetGene(mainHero.professionGenes, heroToAnalyze.professionGenes, searchCriteria.summonProfession)
                     heroToAnalyze.targetProbability = classProbability && professionProbability ? classProbability.value * professionProbability.value : 0
-                } else {
-                    heroToAnalyze.targetProbability = classProbability ? classProbability.value : 0
                 }
             }
 
@@ -173,16 +181,39 @@ const RegressiveSearchPage = () => {
         setView(checked ? 'front' : 'back')
     }
 
-    const sortedHeroes = sortAndFilterHeroes(heroes, filters, sortBy, mutationClass)
+    const sortedHeroes = mainHero ? sortAndFilterHeroes(heroes, filters, sortBy, mainHero.mutationClass) : []
 
     return (
         <Container maxWidth='xl'>
-            <SearchFormSimple defaultSummonClass={mutationClass} isHeroLoaded={!!mainHero} onHeroChange={handleHeroChange} onToggle={handleSearchFormToggle} onSubmit={handleSubmit} />
-            <SortFilter onFiltersChange={handleFiltersChange} onSortByChange={handleSortByChange} onViewToggled={handleViewToggled} visible={!!mainHero} />
-            <LoadingMessage heroCount={heroes.length} loading={isLoading} loaded={hasLoaded} message={loadingMessage} />
+            <SearchFormSimple
+                isSearchEnabled={!!mainHero && !isLoading}
+                onHeroChange={handleHeroChange}
+                onSearch={handleSearch}
+                onSearchCriteriaChange={handleSearchCriteriaChange}
+                onToggle={handleSearchFormToggle}
+                values={searchCriteria} />
+
+            <SortFilter
+                auctionType={searchCriteria.auctionType}
+                onFiltersChange={handleFiltersChange}
+                onSortByChange={handleSortByChange}
+                onViewToggled={handleViewToggled}
+                visible={!!mainHero} />
+
+            <LoadingMessage
+                heroCount={heroes.length}
+                loading={isLoading}
+                loaded={hasLoaded}
+                message={loadingMessage} />
+
             <div className='hero-list'>
-                <MainHero hero={mainHero} view={view} />
-                <SummonsMatchList heroes={sortedHeroes} view={view} highlights={highlights} />
+                <MainHero
+                    hero={mainHero}
+                    view={view} />
+                <SummonsMatchList
+                    heroes={sortedHeroes}
+                    view={view}
+                    highlights={highlights} />
             </div>
         </Container>
     )
